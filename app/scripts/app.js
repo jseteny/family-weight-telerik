d60003d8d4fad1df9458704abe4bbe90ca2d5681
János Setény <jseteny@gmail.com>
2015-08-07 15:26:14 +0000

    Photo Album, Lesson 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,14 @@
-(function() {
+(function () {
     document.addEventListener("deviceready", function () {
-        var app = new kendo.mobile.Application(document.body, { skin: "nova" });
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "nova"
+        });
+
+        $("#images").kendoMobileListView({
+            dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
+            template: "<img src='#: data #'>"
+        });
+
         navigator.splashscreen.hide();
     });
 }());
